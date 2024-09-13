cc10fecf2199aad2b2e661c8f1b619a4828a2776
arupp <arup@pitangent.com>
2024-09-13 08:44:48 +0530

    Initialize the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg'
+
+function App() {
+
+  return (
+    <>
+    <img src={reactLogo} className="logo react" alt="React logo" />
+     <h1>Hello World! </h1>
+    </>
+  )
+}
+
+export default App
